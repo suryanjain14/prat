93549d4fefa4a732ed26eebdb6a842dc68aa51a9
Suryan Jain <suryanjain14@gmail.com>
2021-03-27 14:34:22 +0530

    added oop

--- oop/absclass.py
+++ oop/absclass.py
@@ -8,13 +8,32 @@ class abs(ABC):
         pass
 
 
+class abs2(ABC):
+    def method(self):
+        pass
+
+
+ob = abs2()
+
+
+class test:
+    @abstractmethod
+    def ts(self):
+        pass
+
+
+ob2 = test()
+
+
 #     if you define @abstractmethod and class inherits from ABC then it becomes the abstract class
-#     and its obj cant be created. (@abstractmethod ka poora khel hai babu bhaiya)
+#     and its obj cant be created. (@abstractmethod and ABC ka poora khel hai babu bhaiya)
 
 class sub(abs):
 
     def method(self):
         print('asd')
 
+
 obj = sub()
-ob2 = abs()
+obj.method()
+# ob2 = abs()
